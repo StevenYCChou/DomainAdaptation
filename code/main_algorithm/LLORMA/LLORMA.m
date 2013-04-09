f988a3d36ae85375c760026b59934460c64a577e
Yen-Cheng Chou <chouyencheng@gmail.com>
2013-04-09 17:43:44 +0800

    new LLORMA

--- code/main_algorithm/LLORMA/LLORMA.m
+++ code/main_algorithm/LLORMA/LLORMA.m
@@ -3,11 +3,11 @@
 %Smooth Kernel K_h(s1,s2)
 
 [a, b] = get_anchor(M);
-similarity_row(M,a);
-similarity_col(M,b);
-Epanechnikov_kernel_row
-Epanechnikov_kernel_col
-[U,V] = factorization
+weighted_rows_coef = similarity_row(M,a);
+weighted_cols_coef = similarity_col(M,b);
+filtered_row_coefs = Epanechnikov_kernel_row;
+filtered_col_coefs = Epanechnikov_kernel_col;
+[U,V] = factorization(M, filtered_row_coefs, filtered_col_coefs);
 
 function [a, b] = get_anchor(M)
     a = random('unif', 1, size(M,1));
@@ -20,14 +20,14 @@ function weighted_rows_coef = similarity_row (M,a)
     weighted_coef_rows = row * M_normalized';  
 end
 
-function filtered_row_coefs = Epanechnikov_kernel_row (coefs, bandwidth)
-    filtered_row_coefs = (ones(size(coefs))-coefs.^2) .* (coefs < bandwidth);
-end
-
 function weighted_cols_coef = similarity_col (M,b)
     M_normalized = M ./ repmat( sqrt( sum(M.^2, 1) ) , size(M,1), 1);
     col = M_normalized(:,b);
     weighted_coef_cols = M_normalized' * col;  
+end
+
+function filtered_row_coefs = Epanechnikov_kernel_row (coefs, bandwidth)
+    filtered_row_coefs = (ones(size(coefs))-coefs.^2) .* (coefs < bandwidth);
 end
 
 function filtered_col_coefs = Epanechnikov_kernel_col (coefs, bandwidth)
